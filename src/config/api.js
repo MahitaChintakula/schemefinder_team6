e4cc4342b509fa8742c27b84c3d0b8909361f765
Harini-Upparapalli <24p35a0555@acet.ac.in>
2026-05-11 00:00:11 +0530

    fixed merge conflicts

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+/*<<<<<<< HEAD
 export const API_BASE_URL =
 "https://wingjgs294.execute-api.ap-south-1.amazonaws.com/prod";
 
@@ -27,4 +27,19 @@ export const ENDPOINTS = {
   SAVE_PROFILE:
     `${API_BASE_URL}/save-profile`,
 >>>>>>> 4d83b0f724b603f2e85c53bee4e3a7cc3b33f016
+};*/
+
+export const API_BASE_URL =
+"https://wingjgs294.execute-api.ap-south-1.amazonaws.com/prod";
+
+export const ENDPOINTS = {
+
+  GET_SCHEMES:
+    `${API_BASE_URL}/schemes`,
+
+  MATCH_SCHEMES:
+    `${API_BASE_URL}/match`,
+
+  SAVE_PROFILE:
+    `${API_BASE_URL}/save-profile`,
 };
