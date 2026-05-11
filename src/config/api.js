6058a5cda0b83c94cc4fd6bf00cc8e21fdd44dee
Kathinokkula Bhavani Surya Prabha <kathinokkulabhavani2006@gmail.com>
2026-05-11 11:03:08 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,34 +1,3 @@
-/*<<<<<<< HEAD
-export const API_BASE_URL =
-"https://wingjgs294.execute-api.ap-south-1.amazonaws.com/prod";
-
-export const ENDPOINTS = {
-
-  GET_SCHEMES:
-    `${API_BASE_URL}/schemes`,
-
-  MATCH_SCHEMES:
-    `${API_BASE_URL}/match`,
-
-  SAVE_PROFILE:
-    `${API_BASE_URL}/save-profile`,
-=======
-export const API_BASE_URL =
-"https://wingjgs294.execute-api.ap-south-1.amazonaws.com/prod";
-
-export const ENDPOINTS = {
-
-  GET_SCHEMES:
-    `${API_BASE_URL}/schemes`,
-
-  MATCH_SCHEMES:
-    `${API_BASE_URL}/match`,
-
-  SAVE_PROFILE:
-    `${API_BASE_URL}/save-profile`,
->>>>>>> 4d83b0f724b603f2e85c53bee4e3a7cc3b33f016
-};*/
-
 export const API_BASE_URL =
 "https://wingjgs294.execute-api.ap-south-1.amazonaws.com/prod";
 
