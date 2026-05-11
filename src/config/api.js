2e87eef9931325b265fe8a9cc91bce7dff382e56
Kathinokkula Bhavani Surya Prabha <kathinokkulabhavani2006@gmail.com>
2026-05-11 11:27:04 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL =
-"https://wingjgs294.execute-api.ap-south-1.amazonaws.com/prod";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
 export const ENDPOINTS = {
 
